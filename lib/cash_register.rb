24c963a4f6b01790a801ac7c9a96e49d30047324
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-14 23:53:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def apply_discount
-    if discount
+    if @discount
       @total *= (1 - discount/100)
       puts "After the discount, the total comes to $#{@total}."
     else
