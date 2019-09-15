a8e3945452b1f3805ca545f2481fc3230bf93b79
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-15 00:09:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,9 +25,9 @@ class CashRegister
   def apply_discount
     if @discount != 0
       @total -= @total * (discount/100.to_f)
-      puts "After the discount, the total comes to $#{@total.to_i}."
+      "After the discount, the total comes to $#{@total.to_i}."
     else
-      puts "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
 
